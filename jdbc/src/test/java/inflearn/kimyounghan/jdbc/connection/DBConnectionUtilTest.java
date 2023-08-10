81f293254dd90725619ac598e8ed77e3d5639f89
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-10 11:07:23 +0900

    feat(test): DBConnectionUtil 데이터소스 별 테스트 (#4)
    - DriverManagerDataSource
    - HikariDataSource

--- jdbc/src/test/java/inflearn/kimyounghan/jdbc/connection/DBConnectionUtilTest.java
+++ jdbc/src/test/java/inflearn/kimyounghan/jdbc/connection/DBConnectionUtilTest.java
@@ -1,18 +1,33 @@
 package inflearn.kimyounghan.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Connection;
 
 @Slf4j
 class DBConnectionUtilTest {
 
-    private final DBConnectionUtil dbConnectionUtil = DBConnectionUtil.getInstance();
+    private DBConnectionUtil dbConnectionUtil;
 
     @Test
-    void connection() {
+    void getConnectionDriverManagerDataSource() {
+        dbConnectionUtil = DBConnectionUtil.getInstance(DriverManagerDataSource.class);
+
+        assertConnectionSuccess(dbConnectionUtil);
+    }
+
+    @Test
+    void getConnectionHikariDataSource() {
+        dbConnectionUtil = DBConnectionUtil.getInstance(HikariDataSource.class);
+
+        assertConnectionSuccess(dbConnectionUtil);
+    }
+
+    void assertConnectionSuccess(DBConnectionUtil dbConnectionUtil) {
         Connection conn = dbConnectionUtil.getConnection();
         Assertions.assertThat(conn).isNotNull();
     }
