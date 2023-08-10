7b181a39ccd11c00b48933cb4116cbeee9977504
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-10 11:06:44 +0900

    refactor: DBConnectionUtil 데이터 소스 설정 (#4)
    - getInstance 호출 시 클래스를 넘겨 데이터소스를 지정할 수 있도록 설정

--- jdbc/src/main/java/inflearn/kimyounghan/jdbc/connection/DBConnectionUtil.java
+++ jdbc/src/main/java/inflearn/kimyounghan/jdbc/connection/DBConnectionUtil.java
@@ -1,52 +1,71 @@
 package inflearn.kimyounghan.jdbc.connection;
 
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static inflearn.kimyounghan.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
+@RequiredArgsConstructor
 public class DBConnectionUtil {
 
     private static final DBConnectionUtil INSTANCE = new DBConnectionUtil();
 
-    private final DataSource dataSource;
-
-    private DBConnectionUtil() {
-//        this.dataSource = new DriverManagerDataSource();
-
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(URL);
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
-        this.dataSource = dataSource;
-    }
+    private static DataSource dataSource;
 
     public static DBConnectionUtil getInstance() {
         return INSTANCE;
     }
 
+    public static DBConnectionUtil getInstance(Class<? extends DataSource> datasourceClass) {
+        if (datasourceClass == DriverManagerDataSource.class) {
+            dataSource = getDriverManagerDataSource();
+        } else if (datasourceClass == HikariDataSource.class) {
+            dataSource = getHikariDataSource();
+        } else {
+            throw new IllegalStateException("적절한 DataSource를 찾을 수 없습니다.");
+        }
+        return INSTANCE;
+    }
+
     public Connection getConnection() {
         try {
-//            Connection conn = dataSource.getConnection(USERNAME, PASSWORD);
-            Connection conn = dataSource.getConnection();
+            Connection conn = DataSourceUtils.getConnection(dataSource);
             log.info("connection: {}, class: {}", conn, conn.getClass());
             return conn;
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
     }
 
     public void close(Connection conn, PreparedStatement prepStmt, ResultSet rs) {
-        JdbcUtils.closeConnection(conn);
+        DataSourceUtils.releaseConnection(conn, dataSource);
         JdbcUtils.closeStatement(prepStmt);
         JdbcUtils.closeResultSet(rs);
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    private static DriverManagerDataSource getDriverManagerDataSource() {
+        return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+
+    private static HikariDataSource getHikariDataSource() {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl(URL);
+        hikariDataSource.setUsername(USERNAME);
+        hikariDataSource.setPassword(PASSWORD);
+        return hikariDataSource;
     }
 }
